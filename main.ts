babc2e6ce0225a33011e58e1039fd4ed03d1515b
MariaGSB <121028525+MariaGSB@users.noreply.github.com>
2023-01-19 11:25:48 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+radio.onReceivedString(function (receivedString) {
+    basic.showString("llego")
+})
+radio.setGroup(1)
+radio.setTransmitPower(3)
 basic.forever(function () {
-	
+    radio.sendNumber(input.temperature())
 })
